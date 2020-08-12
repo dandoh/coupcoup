13704ff1cb2cc383c388dfcf1ab183b3b5574d11
Nhan Thai (dandoh) <dainhan605@gmail.com>
2020-08-12 09:39:36 +0700

    ok

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,54 +1,54 @@
-import {Configuration} from "webpack"
+import { Configuration } from "webpack";
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const configuration: Configuration = {
-    devServer: {
-        historyApiFallback: true,
-        port: 6789,
-    },
-    resolve: {
-        extensions: [".mjs", ".js", ".jsx", ".ts", ".tsx"],
-    },
+  devServer: {
+    historyApiFallback: true,
+    port: 6789,
+  },
+  resolve: {
+    extensions: [".mjs", ".js", ".jsx", ".ts", ".tsx"],
+  },
 
-    stats: "errors-warnings",
+  mode: isProduction ? "production" : "development",
 
-    entry: [
-        path.join(__dirname, "src", "index.tsx")
+  stats: isProduction ? undefined : "errors-warnings",
+
+  entry: [path.join(__dirname, "src", "index.tsx")],
+
+  output: {
+    publicPath: "/",
+    path: path.join(__dirname, "build"),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      inject: "body",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              compilerOptions: {
+                noEmit: false,
+              },
+            },
+          },
+        ],
+      },
     ],
+  },
+};
 
-    output: {
-        publicPath: "/",
-        path: path.join(__dirname, "build")
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "index.html",
-            inject: "body"
-        })
-    ],
-
-    module: {
-        rules: [
-            {
-                exclude: /node_modules/,
-                test: /\.tsx?$/,
-                use: [
-                    {
-                        loader: "ts-loader",
-                        options: {
-                            compilerOptions: {
-                                noEmit: false,
-                            },
-                        },
-                    },
-                ],
-            }
-        ]
-    }
-
-}
-
-
-export default configuration
+export default configuration;
